9fe612f30d13c8cddcd176238821433c270c7a7b
Mike Griese <zadjii@gmail.com>
2024-12-17 12:36:40 -0600

    some quick obsidian updates

--- src/extensions/ObsidianExtension/Pages/ObsidianExtensionPage.cs
+++ src/extensions/ObsidianExtension/Pages/ObsidianExtensionPage.cs
@@ -22,6 +22,7 @@ internal sealed partial class ObsidianExtensionPage : ListPage
         Name = "Obsidian Notes";
         PlaceholderText = "Search notes...";
         IsLoading = true;
+        ShowDetails = true;
     }
 
     public override IListItem[] GetItems()
@@ -38,6 +39,7 @@ internal sealed partial class ObsidianExtensionPage : ListPage
             var openNote = new OpenUrlCommand(n.ObsidianProtocolUri)
             {
                 Name = "Open",
+                Result = CommandResult.Dismiss(),
             };
 
             var previewNote = new PreviewNotePage(n);
@@ -49,7 +51,13 @@ internal sealed partial class ObsidianExtensionPage : ListPage
                 MoreCommands = [
                     new CommandContextItem(openNote),
                     new CommandContextItem(new EditNotePage(n)),
+                    new CommandContextItem(new AppendToNotePage(n)),
                 ],
+                Details = new Details()
+                {
+                    Title = n.Name,
+                    Body = n.NoteContent(),
+                },
             };
         });
         return listItems.ToArray();
@@ -109,7 +117,7 @@ public partial class EditNotePage : FormPage
         _note = note;
         Name = "Quick edit";
         Title = _note.Name;
-        Icon = new("\uE70B");
+        Icon = new("\uE70F"); // edit
     }
 
     public override IForm[] Forms() => [new EditNoteForm(_note)];
@@ -171,6 +179,82 @@ public partial class EditNoteForm : Form
         {
             var data = fileContent.ToString();
             File.WriteAllText(_note.AbsolutePath, data);
+        }
+
+        return CommandResult.KeepOpen();
+    }
+}
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "Sample code")]
+public partial class AppendToNotePage : FormPage
+{
+    private readonly Note _note;
+
+    public AppendToNotePage(Note note)
+    {
+        _note = note;
+        Name = "Quick add";
+        Title = _note.Name;
+        Icon = new("\ued0e"); // SubscrptonAdd
+    }
+
+    public override IForm[] Forms() => [new AppendToNoteForm(_note)];
+}
+
+[System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "Sample code")]
+public partial class AppendToNoteForm : Form
+{
+    private readonly Note _note;
+
+    public AppendToNoteForm(Note note)
+    {
+        _note = note;
+        Template = $$"""
+{
+    "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+    "type": "AdaptiveCard",
+    "version": "1.6",
+    "body": [
+        {
+            "type": "TextBlock",
+            "text": "Add to the end of the note...",
+            "wrap": true
+        },
+        {
+            "type": "Input.Text",
+            "id": "Content",
+            "label": "",
+            "isMultiline": true
+        }
+    ],
+    "actions": [
+        {
+            "type": "Action.Submit",
+            "title": "Add",
+            "data": {
+                "id": "Content"
+            }
+        }
+    ]
+}
+        
+"""
+        ;
+    }
+
+    public override ICommandResult SubmitForm(string payload)
+    {
+        var formInput = JsonNode.Parse(payload)?.AsObject();
+        if (formInput == null)
+        {
+            return CommandResult.KeepOpen();
+        }
+
+        var fileContent = formInput["Content"];
+        if (fileContent != null)
+        {
+            var data = fileContent.ToString();
+            File.AppendAllText(_note.AbsolutePath, $"\n{data}");
         }
 
         return CommandResult.KeepOpen();
