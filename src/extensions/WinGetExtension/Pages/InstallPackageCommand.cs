770a486ec23110c3256bfbc74f7db08affb1b2c8
Mike Griese <zadjii@gmail.com>
2025-01-20 14:25:23 -0600

    Start showing messages

--- src/extensions/WinGetExtension/Pages/InstallPackageCommand.cs
+++ src/extensions/WinGetExtension/Pages/InstallPackageCommand.cs
@@ -2,6 +2,8 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.CmdPal.Extensions;
 using Microsoft.CmdPal.Extensions.Helpers;
 using Microsoft.Management.Deployment;
@@ -11,6 +13,8 @@ namespace WinGetExtension.Pages;
 public partial class InstallPackageCommand : InvokableCommand
 {
     private readonly CatalogPackage _package;
+
+    private readonly StatusMessage _installBanner = new();
 
     public InstallPackageCommand(CatalogPackage package)
     {
@@ -23,8 +27,33 @@ public partial class InstallPackageCommand : InvokableCommand
         var result = _package.CheckInstalledStatus();
         if (result.Status == CheckInstalledStatusResultStatus.Ok)
         {
-            var l = result.PackageInstalledStatus;
-            _ = l;
+            var isInstalled = _package.InstalledVersion != null;
+
+            if (isInstalled)
+            {
+                _installBanner.State = MessageState.Info;
+                _installBanner.Message = $"{_package.Name} is already installed";
+                ExtensionHost.ShowStatus(_installBanner);
+
+                // TODO Derp, didn't expose HideStatus from API
+                // _ = Task.Run(() =>
+                // {
+                //    Thread.Sleep(2000);
+                //    ExtensionHost.HideStatus(_installBanner);
+                // });
+            }
+            else
+            {
+                _installBanner.State = MessageState.Info;
+                _installBanner.Message = $"Installing {_package.Name}...";
+                ExtensionHost.ShowStatus(_installBanner);
+                _ = Task.Run(() =>
+                {
+                    Thread.Sleep(2000);
+                    _installBanner.State = MessageState.Success;
+                    _installBanner.Message = $"Successfully installed {_package.Name}";
+                });
+            }
         }
 
         return CommandResult.KeepOpen();
