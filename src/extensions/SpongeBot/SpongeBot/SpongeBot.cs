2e18467296c38e6f03ea9ac48598da71f06de487
Mike Griese <zadjii@gmail.com>
2025-03-28 12:43:35 -0500

    why don't you build in the big sln

---
+++ src/extensions/SpongeBot/SpongeBot/SpongeBot.cs
@@ -0,0 +1,34 @@
+// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Microsoft.CommandPalette.Extensions;
+
+namespace SpongeBot;
+
+[Guid("4e3a46a2-1849-47a5-9c3a-f9a39d15338f")]
+public sealed partial class SpongeBot : IExtension, IDisposable
+{
+    private readonly ManualResetEvent _extensionDisposedEvent;
+
+    private readonly SpongeBotCommandsProvider _provider = new();
+
+    public SpongeBot(ManualResetEvent extensionDisposedEvent)
+    {
+        this._extensionDisposedEvent = extensionDisposedEvent;
+    }
+
+    public object? GetProvider(ProviderType providerType)
+    {
+        return providerType switch
+        {
+            ProviderType.Commands => _provider,
+            _ => null,
+        };
+    }
+
+    public void Dispose() => this._extensionDisposedEvent.Set();
+}
