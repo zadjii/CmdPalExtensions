beea8e273166f0a51d9b1865e63e2d3cdfd6b405
Mike Griese <zadjii@gmail.com>
2025-09-20 06:49:46 -0500

    use shmeuli's winrt server everywhere

--- src/extensions/TemplateExtension/Program.cs
+++ src/extensions/TemplateExtension/Program.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Threading;
 using Microsoft.CommandPalette.Extensions;
+using Shmuelie.WinRTServer;
 
 namespace TemplateExtension;
 
@@ -15,18 +16,14 @@ public class Program
     {
         if (args.Length > 0 && args[0] == "-RegisterProcessAsComServer")
         {
-            using ExtensionServer server = new();
-            var extensionDisposedEvent = new ManualResetEvent(false);
-            var extensionInstance = new SampleExtension(extensionDisposedEvent);
-
-            // We are instantiating an extension instance once above, and returning it every time the callback in RegisterExtension below is called.
-            // This makes sure that only one instance of SampleExtension is alive, which is returned every time the host asks for the IExtension object.
-            // If you want to instantiate a new instance each time the host asks, create the new instance inside the delegate.
-            server.RegisterExtension(() => extensionInstance);
-
-            // This will make the main thread wait until the event is signalled by the extension class.
-            // Since we have single instance of the extension object, we exit as soon as it is disposed.
+            global::Shmuelie.WinRTServer.ComServer server = new();
+            ManualResetEvent extensionDisposedEvent = new(false);
+            TemplateExtension extensionInstance = new(extensionDisposedEvent);
+            server.RegisterClass<TemplateExtension, IExtension>(() => extensionInstance);
+            server.Start();
             extensionDisposedEvent.WaitOne();
+            server.Stop();
+            server.UnsafeDispose();
         }
         else
         {
